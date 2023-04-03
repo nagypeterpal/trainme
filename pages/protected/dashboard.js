21e8269d140065fa9dcd2894ca6decdd894768e2
nagypeterpal <nagypeterpal@gmail.com>
2023-04-03 22:13:48 +0200

    rest

--- pages/protected/dashboard.js
+++ pages/protected/dashboard.js
@@ -216,7 +216,7 @@ export const getServerSideProps = async (ctx) => {
     error: sb_tr_agg_error,
     status: sb_tr_agg_status,
   } = await supabaseServer.rpc("get_training_data_agg", {
-    session.user.id
+    userid: session.user.id,
   });
   if (sb_tr_agg_data) {
     let parsed = JSON.parse(sb_tr_agg_data);
@@ -229,7 +229,7 @@ export const getServerSideProps = async (ctx) => {
     error: sb_tr_det_error,
     status: sb_tr_det_status,
   } = await supabaseServer.rpc("get_training_data_detail", {
-    session.user.id
+    userid: session.user.id,
   });
   if (sb_tr_det_data) {
     let parsed = JSON.parse(sb_tr_det_data);
