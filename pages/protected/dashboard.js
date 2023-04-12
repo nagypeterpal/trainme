09868e56528e756293aad9767df808b3b3fcded5
nagypeterpal <nagypeterpal@gmail.com>
2023-04-12 15:55:02 +0200

    gui update

--- pages/protected/dashboard.js
+++ pages/protected/dashboard.js
@@ -5,6 +5,11 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Avatar from "../../components/Avatar";
+import {
+  UserCircleIcon,
+  XCircleIcon,
+  GlobeAltIcon,
+} from "@heroicons/react/24/solid";
 const TM_PieChart = dynamic(import("../../components/TM_PieChart"), {
   ssr: false,
 });
@@ -21,10 +26,10 @@ export default function Home({ user, training }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="bg-sky-500 hover:bg-sky-400 p-8">
-        <div className="box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white mb-8 p-4 text-6xl w-96">
+        <div className="rounded-2xl border-white border-4 text-white mb-16 p-4 text-6xl w-96">
           TrainMe
         </div>
-        <div className="mb-8 p-4 text-6xl w-48">
+        <div className="mb-2 rounded-2xl bg-white overflow-hidden w-36">
           <Avatar
             uid={user.id}
             url={user.avatarurl}
@@ -32,33 +37,42 @@ export default function Home({ user, training }) {
             readOnly={true}
           />
         </div>
-        <div className="text-white my-4 text-2xl">{user.name}</div>
-        <div>{user.website}</div>
+        <div className="text-white mb-8 text-2xl">
+          {user.name}
+          <div className="text-white  text-sm inline p-1 m-2">
+            <GlobeAltIcon className="h-6 w-6 text-white inline" />
+            <a href="{user.website}">{user.website}</a>
+          </div>
+        </div>
+
         <button
-          className="button block"
+          className="button block text-white rounded-xl border-white  border-2 p-2 m-2 text-center inline text-sm"
           onClick={async () => {
             await router.push("/protected/profile");
           }}
         >
+          <UserCircleIcon className="h-6 w-6 text-white" />
           Profile
         </button>
         <button
-          className="button block"
+          className="button block text-white rounded-xl border-white bg-red-500 border-2 p-2 m-2 inline text-sm"
           onClick={async () => {
             await supabaseClient.auth.signOut();
             router.push("/");
           }}
         >
+          <XCircleIcon className="h-6 w-6 text-white" />
           Sign Out
         </button>
       </div>
 
       <main>
-        <div className="flex flex-col bg-gradient-to-b from-sky-50 to-sky-100">
-          <div className="mx-auto grid grid-cols-2 gap-10">
-            <span className="col-span-2 text-sky-500 text-4xl p-2">
-              Aggragated data
-            </span>
+        <div className="bg-gray-300 hover:bg-gray-500 px-8 text-gray-600 text-4xl p-2">
+          Aggregated training data
+        </div>
+
+        <div className="flex flex-col bg-gradient-to-b from-sky-50 to-sky-100 p-2">
+          <div className="mx-auto grid grid-cols-2 gap-10 max-w-4xl">
             <div className="p-2">
               <TM_PieChart
                 data={[
@@ -79,6 +93,10 @@ export default function Home({ user, training }) {
                 {training.agg.act_pcs_tot}
               </span>
               /<span>{training.agg.pla_pcs_tot}</span>
+              <br />
+              <span className="text-sky-500 text-sm">
+                You have already done this many trainings from the total.
+              </span>
             </div>
 
             <div className="p-2">
@@ -101,6 +119,10 @@ export default function Home({ user, training }) {
                 {training.agg.act_dis_tot}
               </span>
               /<span>{training.agg.pla_dis_tot}</span>
+              <br />
+              <span className="text-sky-500 text-sm">
+                You have already done this many kilometers from the total.
+              </span>
             </div>
 
             <div className="p-2">
@@ -132,6 +154,12 @@ export default function Home({ user, training }) {
                 {training.agg.act_elt_tot - training.agg.act_mot_tot}/
                 {training.agg.pla_tim_tot}
               </span>
+              <br />
+              <span className="text-sky-500 text-sm whitespace-normal">
+                You have already done this many minuts from the total planned
+                trainings, the number in the middle shows the mnutes you were
+                idle while the tracker was running.
+              </span>
             </div>
 
             <div className="p-2">
@@ -145,9 +173,13 @@ export default function Home({ user, training }) {
                 {training.agg.act_spe_max}
               </span>
             </div>
-            <span className="text-sky-500 text-4xl p-2 col-span-2">
-              Detailed training data
-            </span>
+          </div>
+        </div>
+        <div className="bg-gray-300 hover:bg-gray-500 px-8 text-gray-600 text-4xl p-2">
+          Detailed training data
+        </div>
+        <div className="flex flex-col bg-gradient-to-b from-sky-50 to-sky-100 p-2">
+          <div className="mx-auto grid grid-cols-2 gap-10 max-w-4xl">
             {training.lines.map((line) => (
               <div className="p-2 col-span-2" key={line.p_plan_line_id}>
                 <hr />
