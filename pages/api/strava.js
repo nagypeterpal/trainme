d63f8be0b614071a0ddc3ae304c05e5843bb749b
nagypeterpal <nagypeterpal@gmail.com>
2023-06-01 16:28:02 +0200

    ada

--- pages/api/strava.js
+++ pages/api/strava.js
@@ -1,8 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 export default function handler(req, res) {
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL,process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
-  const { error } = await supabase
-  .from('debug')
-  .insert({ txt: req.body });
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL,process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  const { error } = await supabase.from('debug').insert({ txt: req.body });
 }
