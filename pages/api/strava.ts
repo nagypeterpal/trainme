cce8e4e84f28557bed1f8e73413752b6733a7f9c
nagypeterpal <nagypeterpal@gmail.com>
2023-06-01 13:34:52 +0200

    asdas

--- pages/api/strava.ts
+++ pages/api/strava.ts
@@ -1,11 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
+export default function handler(req, res) {
+  const requestMethod = req.method;
+  const body = JSON.parse(req);
+  switch (requestMethod) {
+    case "POST":
+      res
+        .status(200)
+        .json({ message: `You submitted the following data: ${body}` });
 
-export const config = {
-  runtime: "edge",
-};
-
-export default (request: NextRequest) => {
-  return NextResponse.json({
-    name: `Hello, from ${request.url} I'm now an Edge Function!`,
-  });
-};
+    // handle other HTTP methods
+    default:
+      res.status(200).json({ message: `Welcome to API Routes!  ${body}` });
+  }
+}
