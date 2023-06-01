66169005df62d5b5a55e4aad0ca49734d3874e25
nagypeterpal <nagypeterpal@gmail.com>
2023-06-01 16:35:53 +0200

    asd

--- pages/api/strava.js
+++ pages/api/strava.js
@@ -1,6 +1,13 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const saveData = async (whatToSave) => {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  const { error } = await supabase.from("debug").insert({ txt: whatToSave });
+};
 
 export default function handler(req, res) {
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL,process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-  const { error } = await supabase.from('debug').insert({ txt: req.body });
+  saveData(req);
 }
