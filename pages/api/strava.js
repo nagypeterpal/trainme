06281e6bb7ef0755e62d918464d2bb639a17b888
nagypeterpal <nagypeterpal@gmail.com>
2023-06-01 16:39:50 +0200

    sdfsd

--- pages/api/strava.js
+++ pages/api/strava.js
@@ -1,4 +1,5 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+//import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const saveData = async (whatToSave) => {
   const supabase = createClient(
