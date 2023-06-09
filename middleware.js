868dd01a225288fb065ad0213f2b1e0e900c6371
nagypeterpal <nagypeterpal@gmail.com>
2023-06-09 12:25:06 +0200

    middleware

--- middleware.js
+++ middleware.js
@@ -1,17 +1,31 @@
-import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareSupabaseClient({ req, res })
-  const {data: { session },} = await supabase.auth.getSession()
+  const res = NextResponse.next();
+  const supabase = createMiddlewareSupabaseClient({ req, res });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  if (!session) { 
-    if(req.nextUrl.pathname.match('/protected/')){
-      return NextResponse.redirect(new URL('/', req.url))
+  if (!session) {
+    if (req.nextUrl.pathname.match("/protected/")) {
+      return NextResponse.redirect(new URL("/", req.url));
     }
   }
 
   return res;
-
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
